6b8cd7346fa8a412e37f9970e4ca4febd1a24998
RBorisov6 <borisovruslan1512@gmail.com>
2026-03-23 12:27:36 +0300

    Test case explain

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "vector-top-it.hpp"
 
-
 bool testDefaultVector()
 {
   using topit::Vector;
@@ -12,14 +11,15 @@ bool testDefaultVector()
 int main()
 {
   using test_t = bool(*)();
-  test_t tests[] = {
-    testDefaultVector
+  using case_t = std::pair<test_t, const char *>;
+  case_t tests[] = {
+    {testDefaultVector, "Default constructed should be empty"}
   };
 
-  size_t size = sizeof(tests) / sizeof(test_t);
+  size_t size = sizeof(tests) / sizeof(case_t);
   std::cout << std::boolalpha;
   for (size_t i = 0; i < size ; ++i)
   {
-    std::cout << tests[i]() << ": " << i << '\n';
+    std::cout << tests[i].first() << ": " << tests[i].second << '\n';
   }
 }
