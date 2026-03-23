310b19408d37b4fe5424f35ab130d2d655a57946
RBorisov6 <borisovruslan1512@gmail.com>
2026-03-23 13:00:07 +0300

    const access tests failed

--- main.cpp
+++ main.cpp
@@ -1,23 +1,112 @@
+
 #include <iostream>
 #include "vector-top-it.hpp"
 
-bool testDefaultVector()
+using topit::Vector;
+bool test1()
 {
-  using topit::Vector;
   Vector< int > v;
   return v.isEmpty();
+}
+
+bool test2()
+{
+  Vector< int > v;
+  return v.getSize() == 0;
+}
+
+bool test3()
+{
+  size_t size = 3ull;
+  Vector< int > v(size);
+  return v.getSize() == size;
+}
+
+bool test4()
+{
+  constexpr size_t size = 3ull;
+  Vector< int > v(size);
+  try
+  {
+    v.at(0);
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool test5()
+{
+  constexpr size_t size = 3ull;
+  Vector< int > v(size);
+  try
+  {
+    v.at(size + 1);
+    return false;
+  }
+  catch (const std::out_of_range&)
+  {
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool test6()
+{
+  constexpr size_t size = 3ull;
+  const Vector< int > v(size);
+  try
+  {
+    v.at(0);
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
+}
+
+bool test7()
+{
+  constexpr size_t size = 3ull;
+  const Vector< int > v(size);
+  try
+  {
+    v.at(size + 1);
+    return false;
+  }
+  catch (const std::out_of_range&)
+  {
+    return true;
+  }
+  catch (...)
+  {
+    return false;
+  }
 }
 
 int main()
 {
   using test_t = bool(*)();
-  using case_t = std::pair<test_t, const char *>;
+  using case_t = std::pair< test_t, const char* >;
   case_t tests[] = {
-    {testDefaultVector, "Default constructed should be empty"}
+    {test1, "Default constructed vector is empty"},
+    {test2, "Defaul constructed vector size is zero"},
+    {test3, "Vector constructed with size has non-zero size"},
+    {test4, "In range access does not generate exceptions"},
+    {test5, "Out of range access generates std::out_of_range exception"},
+    {test6, "In range access for const vector does not generate exceptions"},
+    {test7, "Out of range access for const vector generates std::out_of_range exception"}
   };
 
   size_t size = sizeof(tests) / sizeof(case_t);
   std::cout << std::boolalpha;
+
   for (size_t i = 0; i < size ; ++i)
   {
     std::cout << tests[i].first() << ": " << tests[i].second << '\n';
--- vector-top-it.hpp
+++ vector-top-it.hpp
@@ -1,3 +1,4 @@
+
 #ifndef VECTOR_TOP_IT_HPP
 #define VECTOR_TOP_IT_HPP
 #include <cstddef>
@@ -7,12 +8,19 @@ namespace topit
   struct Vector
   {
     Vector();
+    explicit Vector(size_t size);
     ~Vector();
 
     Vector(const Vector< T >&) = delete;
     Vector< T >& operator=(const Vector< T >&) = delete;
 
     bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
+
+    T& operator[](size_t id) noexcept;
+    const T& operator[](size_t id) const noexcept;
+    T& at(size_t id);
+    const T& at(size_t id) const;
 
   private:
     T* data_;
@@ -28,6 +36,13 @@ topit::Vector< T >::Vector():
 {}
 
 template< class T >
+topit::Vector< T >::Vector(size_t size):
+  data_(size ? new T[size] : nullptr),
+  size_(size),
+  capacity_(size)
+{}
+
+template< class T >
 topit::Vector< T >::~Vector()
 {
   delete[] data_;
@@ -36,7 +51,37 @@ topit::Vector< T >::~Vector()
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
 {
-  return false;
+  return !size_;
+}
+
+template< class T>
+size_t topit::Vector< T >::getSize() const noexcept
+{
+  return size_;
+}
+
+template< class T >
+T& topit::Vector< T >::operator[](size_t id) noexcept
+{
+
+}
+
+template< class T >
+T& topit::Vector< T >::at(size_t id)
+{
+  throw std::logic_error("Bad id");
+}
+
+template< class T >
+const T& topit::Vector< T >::operator[](size_t id) const noexcept
+{
+
+}
+
+template< class T >
+const T& topit::Vector< T >::at(size_t id) const
+{
+  throw std::logic_error("Bad id");
 }
 
 #endif
